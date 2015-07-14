18a10f797576a487c9cc696871a6149b34ec502b
Giannina Calcagno <giannina@dot175.com>
2015-07-14 12:39:54 -0300

    primer commit - unificacion + step 6

---
+++ step6.php
@@ -0,0 +1,110 @@
+
+<link rel="stylesheet" href="cargador/ezequiel.css">
+
+<div class="crm_contenedorA">
+
+	<div class="columna50">
+
+		<div class="columna50 columna-agregar-campo columna-background">
+			<div class="titulo-general">
+				<h3>Agregar / Modificar Campo</h3>
+			</div>
+			<div class="form">
+				<form action="">
+					<label> Nombre</label>
+					<input type="text" name="" value="">
+					<label> Tipo de campo</label>
+					<select name="">
+						<option value="" disabled selected>domicilio</option>
+						<option value="">Opciones</option>
+						<option value="">Opciones</option>
+						<option value="">Opciones</option>
+					</select>
+				</form>
+
+				
+
+			</div>
+			<div class="subcampos">
+				<p>subcampos</p>
+				<ul>
+					<li></li>
+					<li>Calle</li>
+					<li>Numero</li>
+					<li>Piso</li>
+					<li>Depto</li>
+					<li>Torre</li>
+					<li>Localidad</li>
+					<li>Provincia</li>
+					<li>Pais</li>
+					<li>CP</li>
+				</ul>
+			</div>
+			
+
+			<button class="boton">Aceptar</button>
+
+		</div>
+
+		<div class="columna-auto columna-background">
+			<div class="titulo-general">
+				<h3>Elegir Campo Clave</h3>
+			</div>
+
+			<div class="form-CRM">
+				<form action="">
+					<label> Campo clave del CRM</label>
+					<select name="">
+						<option value="" disabled selected></option>
+						<option value="">item</option>
+						<option value="">item</option>
+						<option value="">item</option>
+						<option value="">item</option>
+						<option value="">item</option>
+						<option value="">item</option>
+					</select>
+				</form>
+			</div>
+
+			<button class="boton">Seleccionar</button>
+		</div>
+	</div>
+
+	<div class="columna50 columna-background columna-tabla">
+
+			<table border="1">
+		        <thead>
+		            <tr>
+		                <th>Campo</th>
+		                <th>Tipo de campo</th>
+		                <th>Acciones</th>
+		            </tr>
+		        </thead>
+		        <tbody>
+		            <tr>
+		                <td>nombre</td>
+		                <td>texto</td>
+		                <td></td>
+		            </tr>
+		             <tr>
+		                <td>apellido</td>
+		                <td>texto</td>
+		                <td></td>
+		            </tr>
+		             <tr>
+		                <td>Telefono1</td>
+		                <td>telefono</td>
+		                <td></td>
+		            </tr>
+		             <tr>
+		                <td>domicilio1</td>
+		                <td>domicilio</td>
+		                <td></td>
+		            </tr>
+		        </tbody>
+		    </table>
+		</div>
+
+	
+</div>
+
