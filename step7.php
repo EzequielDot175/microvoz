ae3c57779f455e21a301a664605548bbd4aed344
Giannina Calcagno <giannina@dot175.com>
2015-07-14 16:33:40 -0300

    unificacion

---
+++ step7.php
@@ -0,0 +1,69 @@
+
+<link rel="stylesheet" href="cargador/ezequiel.css">
+
+<div class="crm_contenedorA">
+
+	<div class="form-grafico">
+
+		<div class="">
+
+			<div class="form">
+				<form action="">
+
+					<label class="simple">Nombre</label>
+					<input type="text" name="" value="">
+					<label class="simple ">Módulo</label>
+					<input class="gris-background" type="text" name="" value="">
+					<label class="simple">Entidad</label>
+					<input class="gris-background" type="text" name="" value="">
+					<label class="simple">Campaña</label>
+					<input class="gris-background" type="text" name="" value="">
+					<label class="simple">Estado</label>
+					<select class="gris-background" name="">
+						<option value="" disabled selected>domicilio</option>
+						<option value="">Opciones</option>
+						<option value="">Opciones</option>
+						<option value="">Opciones</option>
+					</select>
+					<label >Fecha de vencimiento</label>
+					<input type="text" name="" value="">
+					
+					<div class="separador"></div>
+
+					<label>Tickets Total</label>
+					<input class="gris-background" type="text" name="" value="">
+					<label >Tickets Abiertos</label>
+					<input class="gris-background" type="text" name="" value="">
+					<label >Tickets Cerrados</label>
+					<input class="gris-background" type="text" name="" value="">
+
+				</form>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="grafico">
+
+		<div class="titulo">
+			<h3>Datos de la carga de tickets</h3>
+		</div>
+
+		<div class="grafico-torta gris-background">
+			<h3>Total de contactos: 229.000</h3>
+		</div>
+
+		<div class="descripcion gris-background">
+			<p>Tipo de ticket: Garbarino Mora temprana</p>
+			<p>Campaña: Garbarino Mora temprana</p>
+			<p>Campo clave: DNI</p>
+			<p>Lote:197</p>
+			<p>Nombre de archivo: asig2015.xls</p>
+		</div>
+
+	</div>
+
+<button class="boton">Aceptar</button><button class="boton">Cancelar</button>
+
+</div>
+
