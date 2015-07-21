742a07dbc3a2576a4b930d40449ad0ee8d020863
=Ezequiel Romero <=w-eze-e@hotmail.com>
2015-07-21 17:34:25 -0300

    index fuera

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
 	function findSteps($action = null){
 		$files = scandir("./");
 		foreach($files as $k => $v):
 			if(preg_match("/.php/", $v) > 0):
-				echo('<li><a href="./?file='.$v.'">'.$v.'</a></li>');
+				if ($v != "index.php") {
+					echo('<li><a href="./?file='.$v.'">'.$v.'</a></li>');
+				}
 			endif;
 		endforeach;
 	}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
